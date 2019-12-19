07586242a7cf5f8d629bec638d9846cd2e1e2e4b
Carlos Xilotl <cxilotl@aol.com>
2019-12-19 12:44:56 +0000

    setting up css modules

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import cssStyles from './app.css';
+
 // source goes here
 const app = () => {
   const rootEl = document.getElementById('root');
-  const title = 'This is my React test';
+  const title = 'Which? Javascript exercise';
   ReactDOM.render(
     <div>
-      <div>{ title }</div>
-      <div>
+      <h1>{ title }</h1>
+      <div className={ cssStyles.layout }>
         Test
       </div>
     </div>,
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,43 @@
 const path = require('path');
 
-module.exports = {
-  entry: {
-    app: ['./src/index.js']
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/assets/',
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader',
-          'eslint-loader'
-        ]
-      }
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  }
+module.exports =  () => {
+
+  return {
+    entry: {
+      app: ['./src/index.js']
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/assets/',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            }
+          ],
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            'babel-loader',
+            'eslint-loader'
+          ]
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    devtool: 'inline-source-map',
+  };
 };
